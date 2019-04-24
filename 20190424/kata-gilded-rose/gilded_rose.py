8f09041825dad34c04c8488333a4cd9b288cd338
Ronan Amicel <ronan.amicel@gmail.com>
2019-04-24 20:42:18 +0200

    Remaniement: on élimine tout de suite le cas particulier des Sulfuras

--- 20190424/kata-gilded-rose/gilded_rose.py
+++ 20190424/kata-gilded-rose/gilded_rose.py
@@ -12,12 +12,13 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            if item.name == SULFURAS:
+                continue
             self.decrease_sell_in(item)
             self.update_item_quality(item)
 
     def decrease_sell_in(self, item):
-        if item.name != SULFURAS:
-            item.sell_in = item.sell_in - 1
+        item.sell_in = item.sell_in - 1
 
     def update_item_quality(self, item):
         if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS:
@@ -43,9 +44,8 @@ class GildedRose(object):
             item.quality = item.quality + 1
 
     def decrease_quality(self, item):
-        if item.name != SULFURAS:
-            if item.quality > 0:
-                item.quality = item.quality - 1
+        if item.quality > 0:
+            item.quality = item.quality - 1
 
 
 class Item:
